8ee59bae79c90eb9f49f1aa0552f0482dac29d02
sishida21 <29348731+sishida21@users.noreply.github.com>
2024-01-30 18:48:46 +0900

    feat: simplify script

--- calculate_scores.py
+++ calculate_scores.py
@@ -11,17 +11,12 @@ from rdkit.ML.Scoring import Scoring
 from rdkit.Chem.Scaffolds import MurckoScaffold
 
 
-def _merge_files(dirname: str, useGraphSim: bool =False, radiusGraphSim: int =2):
+def _merge_files(dirname: str):
     def _extract_number(fname):
         base, _ = os.path.splitext(fname)
         return int(base.split("_active")[-1])
-
-    if useGraphSim:
-        files = glob.glob(os.path.join(dirname, f"gesims_r{radiusGraphSim}_active*.txt"))
-    else:  # tanimoto
-        files = glob.glob(os.path.join(dirname, f"tanimoto_sims_active*.txt"))
+    files = glob.glob(os.path.join(dirname, f"*active*.txt"))
     files_sorted = sorted(files, key=_extract_number)
-    print(files_sorted)
 
     with open(files_sorted[0], 'r') as f:
         num_lines = len(f.readlines())
@@ -34,29 +29,21 @@ def _merge_files(dirname: str, useGraphSim: bool =False, radiusGraphSim: int =2)
         for i, l in enumerate(lines):
             _, value = l.split('\t')
             merged_results[i].append(value.strip('\n'))
-    if useGraphSim:
-        ofname = os.path.join(dirname, f"merged_results_r{radiusGraphSim}.txt")
-    else:
-        ofname = os.path.join(dirname, f"merged_results.txt")
+    ofname = os.path.join(dirname, f"merged_results.txt")
     print(f"[INFO] Dump file: {ofname}")
     with open(ofname, 'w') as f:
         f.write('\n'.join('\t'.join(r) for r in merged_results))
 
 
-def _create_dataframe_from_files(dirname: str, useGraphSim: bool =False, radiusGraphSim: int =2) -> pl.DataFrame:
-    if useGraphSim:
-        num_columns = len(glob.glob(os.path.join(dirname, f"gesims_r{radiusGraphSim}_active*.txt")))
-        fname = os.path.join(dirname, f"merged_results_r{radiusGraphSim}.txt")
-    else:  # tanimoto
-        num_columns = len(glob.glob(os.path.join(dirname, f"tanimoto_sims_active*.txt")))
-        fname = os.path.join(dirname, "merged_results.txt")
+def _create_dataframe_from_files(dirname: str) -> pl.DataFrame:
+    num_columns = len(glob.glob(os.path.join(dirname, f"*active*.txt")))
+    fname = os.path.join(dirname, "merged_results.txt")
     df = pl.read_csv(
         fname,
         has_header=False,
         separator="\t",
         new_columns=['index'] + [str(i) for i in range(num_columns)],
         dtypes=[pl.Int32] + [pl.Float32 for _ in range(num_columns)])
-
     column_names = df.columns
     column_names.remove('index')
     df = df.with_columns((pl.col('index').is_in([int(i) for i in column_names])).alias('is_active'))
@@ -67,20 +54,15 @@ def _create_dataframe_from_files(dirname: str, useGraphSim: bool =False, radiusG
 def _calculate_ef_bedroc_auc(
     dirname: str,
     trial_num: int =50,
-    sample_num: int =10,
+    sample_num: int =5,
     fractions: List[float] = [0.05],
-    alpha: int =20,
-    useGraphSim: bool =False,
-    radiusGraphSim: int =2) -> Dict[str, List[float]]:
+    alpha: int =20) -> Dict[str, List[float]]:
 
     ef_list = []
     bedroc_list = []
     auc_list = []
 
-    df = _create_dataframe_from_files(
-        dirname,
-        useGraphSim=useGraphSim,
-        radiusGraphSim=radiusGraphSim)
+    df = _create_dataframe_from_files(dirname)
     active_column_names = [c for c in df.columns if c not in {'index', 'is_active'}]
     for _ in range(trial_num):
         sampled_columns = random.sample(active_column_names, sample_num)
@@ -114,17 +96,12 @@ def _calc_scaffoldEF(
     dirname: str,
     scaffolds: np.array(str),
     fraction: float =0.05,
-    sample_num: int =10,
-    trial_num: int =50,
-    useGraphSim: bool=False,
-    radiusGraphSim: int=2)->float:
+    sample_num: int =5,
+    trial_num: int =50)->float:
     
     scaffold_ef_list = []
 
-    df = _create_dataframe_from_files(
-        dirname,
-        useGraphSim=useGraphSim,
-        radiusGraphSim=radiusGraphSim)
+    df = _create_dataframe_from_files(dirname)
 
     total_num = scaffolds.shape[0]
     total_scaffolds_num = np.unique(scaffolds).shape[0]
@@ -166,83 +143,46 @@ def get_scaffolds_from_target_name(dataset_dir: str) -> np.array(str):
 
 def main():
     trial_num = 50
-    sample_num = 10
-    radius_list = [1, 2, 3, 4]
-    dataset_root_dir = "./data/benchmarking_platform/compounds/MUV/"
-    result_dir_gesim = "./result/benchmarking_platform/MUV/ge_sim"
-    result_dir_tanimoto = "./result/benchmarking_platform/MUV/tanimoto_sim/"
-    base_dir_list = [result_dir_gesim, result_dir_tanimoto]
+    sample_num = 5
+    dataset_type = ["ChEMBL", "MUV", "DUD"]
     alpha_list = [20, 100]
     fractions_list = [[0.05], [0.01]]
     do_merge_files = True
 
-    for base_dir in base_dir_list:
-        print(base_dir)
-        target_dirs = [d for d in glob.glob(os.path.join(base_dir, "result_*")) if os.path.isdir(d)]
-        target_dirs.sort()
+    for dtype in dataset_type:
+        dataset_root_dir = os.path.join("./data/benchmarking_platform/compounds/", dtype)
+        base_dir_list = [d for d in glob.glob(os.path.join("./result/benchmarking_platform/", dtype, "*")) if os.path.isdir(d)]
         
-        if base_dir == result_dir_gesim:
-            for alpha, fractions in zip(alpha_list, fractions_list):
-                for r in radius_list:
-                    result_dict = {}
-                    for target_dir in target_dirs:
-                        target_name = os.path.basename(target_dir).split("_", 1)[1]
-                        print(target_name)
-                        if do_merge_files:
-                            _merge_files(target_dir, useGraphSim=True, radiusGraphSim=r)
-                        result = _calculate_ef_bedroc_auc(
-                            target_dir,
-                            trial_num=trial_num,
-                            sample_num=sample_num,
-                            alpha=alpha,
-                            fractions=fractions,
-                            useGraphSim=True,
-                            radiusGraphSim=r)
-                        scaffolds = get_scaffolds_from_target_name(os.path.join(dataset_root_dir, target_name))
-                        result.update(_calc_scaffoldEF(
-                            target_dir,
-                            scaffolds=scaffolds,
-                            fraction=fractions[0],
-                            trial_num=trial_num,
-                            sample_num=sample_num,
-                            useGraphSim=True,
-                            radiusGraphSim=r
-                        ))
-                        result_dict[target_name] = result
-                    ofname = f"{base_dir}/result_r{r}_a{alpha}_f{str(fractions[0]).replace('.', '')}.pkl"
-                    with open(ofname, 'wb') as f:
-                        pickle.dump(result_dict, f)
-        else:  # "./result/tanimoto_sim"
+        for base_dir in base_dir_list:
+            print(base_dir)
+            target_dirs = [d for d in glob.glob(os.path.join(base_dir, "result_*")) if os.path.isdir(d)]
+            target_dirs.sort()
+
             for alpha, fractions in zip(alpha_list, fractions_list):
                 result_dict = {}
                 for target_dir in target_dirs:
                     target_name = os.path.basename(target_dir).split("_", 1)[1]
                     print(target_name)
                     if do_merge_files:
-                        _merge_files(target_dir, useGraphSim=False, radiusGraphSim=None)
+                        _merge_files(target_dir)
                     result = _calculate_ef_bedroc_auc(
                         target_dir,
                         trial_num=trial_num,
                         sample_num=sample_num,
                         alpha=alpha,
-                        fractions=fractions,
-                        useGraphSim=False,
-                        radiusGraphSim=None)
+                        fractions=fractions)
                     scaffolds = get_scaffolds_from_target_name(os.path.join(dataset_root_dir, target_name))
                     result.update(_calc_scaffoldEF(
                         target_dir,
                         scaffolds=scaffolds,
                         fraction=fractions[0],
                         trial_num=trial_num,
-                        sample_num=sample_num,
-                        useGraphSim=False,
-                        radiusGraphSim=None
-                    ))
+                        sample_num=sample_num))
                     result_dict[target_name] = result
                 ofname = f"{base_dir}/result_a{alpha}_f{str(fractions[0]).replace('.', '')}.pkl"
                 with open(ofname, 'wb') as f:
                     pickle.dump(result_dict, f)
-
     
+
 if __name__ == "__main__":
     main()

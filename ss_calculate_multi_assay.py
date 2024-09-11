c00ef38cae71622038628130c65a05d291eaccac
sishida21 <29348731+sishida21@users.noreply.github.com>
2024-09-11 11:39:21 +0900

    feat: enable to parallelize the processes

--- ss_calculate_multi_assay.py
+++ ss_calculate_multi_assay.py
@@ -3,6 +3,7 @@ import pickle
 import os
 import re
 
+from joblib import Parallel, delayed
 from rdkit import Chem, DataStructs
 from rdkit.Chem import AllChem, MACCSkeys
 from rdkit.Chem.AtomPairs import Pairs, Torsions
@@ -13,6 +14,75 @@ from gesim import gesim
 
 def extract_number(f):
     return int(re.search(r'\d+', f).group())
+
+
+def process_assay_file(trial_num, fname, id_mol_dict):
+    result_dict = {
+        "mfp": [],
+        "fcfp": [],
+        "maccs": [],
+        "apfp": [],
+        "ttfp": [],
+        "gesim": [],
+    }
+
+    with open(fname, 'r') as f:
+        lines = [l.strip('\n') for l in f.readlines()]
+
+    used_num_multi = 3629
+    ref_rank = range(4, 0, -1)
+    for l in lines[:used_num_multi]:
+        sim_rank_list = l.split()
+        sim_mol_rank_list = [id_mol_dict[i] for i in sim_rank_list]
+
+        # Morgan Fingerprint (r=2, dim=2048)
+        fps = [AllChem.GetMorganFingerprintAsBitVect(m, radius=2, nBits=2048) for m in sim_mol_rank_list]
+        sim_list = DataStructs.BulkTanimotoSimilarity(fps[0], fps[1:])
+        if len(set(sim_list)) == 1:
+            result_dict['mfp'].append(0)
+        else: 
+            result_dict['mfp'].append(spearmanr(ref_rank, sim_list).statistic)
+
+        # Feature-connectivity Fingerprint (r=2, dim=2048)
+        fps = [AllChem.GetMorganFingerprintAsBitVect(m, radius=2, nBits=2048, useFeatures=True) for m in sim_mol_rank_list]
+        sim_list = DataStructs.BulkTanimotoSimilarity(fps[0], fps[1:])
+        if len(set(sim_list)) == 1:
+            result_dict['fcfp'].append(0)
+        else:
+            result_dict['fcfp'].append(spearmanr(ref_rank, sim_list).statistic)
+
+        # MACCS keys
+        fps = [MACCSkeys.GenMACCSKeys(m) for m in sim_mol_rank_list]
+        sim_list = DataStructs.BulkTanimotoSimilarity(fps[0], fps[1:])
+        if len(set(sim_list)) == 1:
+            result_dict['maccs'].append(0)
+        else:
+            result_dict['maccs'].append(spearmanr(ref_rank, sim_list).statistic)
+
+        # Atom Pair Fingerprint
+        fps = [Pairs.GetAtomPairFingerprint(m) for m in sim_mol_rank_list]
+        sim_list = DataStructs.BulkTanimotoSimilarity(fps[0], fps[1:])
+        if len(set(sim_list)) == 1:
+            result_dict['apfp'].append(0)
+        else:
+            result_dict['apfp'].append(spearmanr(ref_rank, sim_list).statistic)
+
+        # Topological Torsion Fingerprint
+        fps = [Torsions.GetTopologicalTorsionFingerprintAsIntVect(m) for m in sim_mol_rank_list]
+        sim_list = DataStructs.BulkTanimotoSimilarity(fps[0], fps[1:])
+        if len(set(sim_list)) == 1:
+            result_dict['ttfp'].append(0)
+        else:
+            result_dict['ttfp'].append(spearmanr(ref_rank, sim_list).statistic)
+
+        # GESim
+        sim_list = gesim.graph_entropy_similarity_batch(sim_mol_rank_list[0], sim_mol_rank_list[1:], r=4)
+        if len(set(sim_list)) == 1:
+            result_dict['gesim'].append(0)
+        else:
+            result_dict['gesim'].append(spearmanr(ref_rank, sim_list).statistic)
+    
+    return trial_num, result_dict
 
 
 def main():
@@ -42,73 +112,15 @@ def main():
         "apfp": [[] for _ in range(num_use_files)],
         "ttfp": [[] for _ in range(num_use_files)],
         "gesim": [[] for _ in range(num_use_files)],
-        "gesim_au": [[] for _ in range(num_use_files)],
-        "gesim_r3": [[] for _ in range(num_use_files)],
     }
 
-    for trial_num, fname in enumerate(files[:num_use_files]):
-        with open(fname, 'r') as f:
-            lines = [l.strip('\n') for l in f.readlines()]
+    num_cores = 50
+    results = Parallel(n_jobs=num_cores)(delayed(process_assay_file)(i, fname, id_mol_dict) for i, fname in enumerate(files[:num_use_files]))
 
-        used_num_multi = 3629
-        ref_rank = range(4, 0, -1)
-        for l in lines[:used_num_multi]:
-            sim_rank_list = l.split()
-            sim_mol_rank_list = [id_mol_dict[i] for i in sim_rank_list]
+    for trial_num, result in results:
+        for k in result_dict.keys():
+            result_dict[k][trial_num] = result[k]
 
-            # Morgan Fingerprint (r=2, dim=2048)
-            fps = [AllChem.GetMorganFingerprintAsBitVect(m, radius=2, nBits=2048) for m in sim_mol_rank_list]
-            sim_list = DataStructs.BulkTanimotoSimilarity(fps[0], fps[1:])
-            if len(set(sim_list)) == 1:
-                result_dict['mfp'][trial_num].append(0)
-            else: 
-                result_dict['mfp'][trial_num].append(spearmanr(ref_rank, sim_list).statistic)
-
-            # Feature-connectivity Fingerprint (r=2, dim=2048)
-            fps = [AllChem.GetMorganFingerprintAsBitVect(m, radius=2, nBits=2048, useFeatures=True) for m in sim_mol_rank_list]
-            sim_list = DataStructs.BulkTanimotoSimilarity(fps[0], fps[1:])
-            if len(set(sim_list)) == 1:
-                result_dict['fcfp'][trial_num].append(0)
-            else:
-                result_dict['fcfp'][trial_num].append(spearmanr(ref_rank, sim_list).statistic)
-
-            # MACCS keys
-            fps = [MACCSkeys.GenMACCSKeys(m) for m in sim_mol_rank_list]
-            sim_list = DataStructs.BulkTanimotoSimilarity(fps[0], fps[1:])
-            if len(set(sim_list)) == 1:
-                result_dict['maccs'][trial_num].append(0)
-            else:
-                result_dict['maccs'][trial_num].append(spearmanr(ref_rank, sim_list).statistic)
-
-            # Atom Pair Fingerprint
-            fps = [Pairs.GetAtomPairFingerprint(m) for m in sim_mol_rank_list]
-            sim_list = DataStructs.BulkTanimotoSimilarity(fps[0], fps[1:])
-            if len(set(sim_list)) == 1:
-                result_dict['apfp'][trial_num].append(0)
-            else:
-                result_dict['apfp'][trial_num].append(spearmanr(ref_rank, sim_list).statistic)
-
-            # Topological Torsion Fingerprint
-            fps = [Torsions.GetTopologicalTorsionFingerprintAsIntVect(m) for m in sim_mol_rank_list]
-            sim_list = DataStructs.BulkTanimotoSimilarity(fps[0], fps[1:])
-            if len(set(sim_list)) == 1:
-                result_dict['ttfp'][trial_num].append(0)
-            else:
-                result_dict['ttfp'][trial_num].append(spearmanr(ref_rank, sim_list).statistic)
-
-            # GESim
-            sim_list = gesim.graph_entropy_similarity_batch(sim_mol_rank_list[0], sim_mol_rank_list[1:], r=4, uncertainty=1)
-            if len(set(sim_list)) == 1:
-                result_dict['gesim'][trial_num].append(0)
-            else:
-                result_dict['gesim'][trial_num].append(spearmanr(ref_rank, sim_list).statistic)
-
-            # GESim (r=3)
-            sim_list = gesim.graph_entropy_similarity_batch(sim_mol_rank_list[0], sim_mol_rank_list[1:], r=3, uncertainty=1)
-            if len(set(sim_list)) == 1:
-                result_dict['gesim_r3'][trial_num].append(0)
-            else:
-                result_dict['gesim_r3'][trial_num].append(spearmanr(ref_rank, sim_list).statistic)
 
     output_file = 'result/similaritybenchmark/result_multi_assay.pkl'
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
